ef7f246d9d863f9117e9a1b7573b4a78a668a307
Ethan Holleman <eholleman@luc.edu>
2021-01-25 13:56:25 -0800

    Allow for running jobs with same ligand ensemble for multible protein targets

--- handler/args.py
+++ handler/args.py
@@ -5,7 +5,7 @@ from pathlib import Path
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-l', '--ligands', help='Path to ligand directory.')
-    parser.add_argument('-p', '--protein', help='Path to protein (pdb) file without ligand.')
+    parser.add_argument('-p', '--protein', help='Path to protein (pdb) file (or directory) without ligand.')
     parser.add_argument('-o', '--parent', help='Parent directory that all results are written under.')
     parser.add_argument('-e', '--exe', help='Path to RosettaScripts exe')
     parser.add_argument('-m', '--moist', action='store_true', help='Moist run. Do everything normally but only submit the first job.')
--- rh.py
+++ rh.py
@@ -3,6 +3,7 @@ from handler.docking import DockJob
 from handler.args import get_args
 from handler import make_logger
 from handler import DEFAULT_XML
+from pathlib import Path
 
 
 logger = make_logger(__name__)
@@ -11,7 +12,8 @@ logger = make_logger(__name__)
 def main():
     logger.info('Started run')
     args = get_args()
-    pretty_args = ' '.join(['{}: {}'.format(arg, getattr(args, arg)) for arg in vars(args)])
+    pretty_args = ' '.join(
+        ['{}: {}'.format(arg, getattr(args, arg)) for arg in vars(args)])
     logger.info('Submitted following arguments: {}'.format(pretty_args))
     ligand_descriptors = LigandDescriptor.generate_from_directory(args.ligands)
     DockJob.rosetta_exe = args.exe
@@ -19,13 +21,34 @@ def main():
         xml_template = args.xml_template
     else:
         xml_template = DEFAULT_XML
-    jobs = (DockJob(
-        args.parent.joinpath(ligand.name), 
-        ligand, 
-        protein=args.protein,
-        number_iterations=args.num_iters,
-        xml_template=xml_template) 
-    for ligand in ligand_descriptors)
+
+    protein = Path(args.protein)
+    if protein.is_dir():  # if is a directory find all pdb files in this dir
+        protein = [pdb for pdb in protein.iterdir() if pdb.suffix == '.pdb']
+    jobs = []
+    if isinstance(protein, list):
+        # multiple proteins to deal with so need to create directories for
+        # each of these proteins
+        for pdb_path in protein:
+            # this assumes unique filename for each pdb
+            pdb_dir = args.parent.joinpath(pdb_path.stem)
+            pdb_dir.mkdir()
+            for ligand in ligand_descriptors:
+                jobs.append(
+                    DockJob(
+                        pdb_dir.joinpath(ligand.name), ligand, protein=pdb_path,
+                        number_iterations=args.num_iters,
+                        xml_template=xml_template
+                    )
+                )
+    else:  # only need to create jobs for one protein
+        jobs = (DockJob(
+            args.parent.joinpath(ligand.name),
+            ligand,
+            protein=args.protein,
+            number_iterations=args.num_iters,
+            xml_template=xml_template)
+            for ligand in ligand_descriptors)
 
     if args.moist:  # only run the one job (usually for testing)
         logger.info('Running moist')
@@ -46,6 +69,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-        
-
-
