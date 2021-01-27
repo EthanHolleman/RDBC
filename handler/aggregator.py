42e7ff420b9a40289fdaf306d1a1f1b46edbd3ce
Ethan Thomas Holleman <ethollem@farm.cse.ucdavis.edu>
2021-01-26 20:43:45 -0800

    Debug aggregator function on farm

--- handler/aggregator.py
+++ handler/aggregator.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import re
 
 
+
 def process_score_file_line(line):
     '''Process a Rosetta score file line into a list.
 
@@ -13,6 +14,8 @@ def process_score_file_line(line):
         list: List of items in the line string.
     '''
     l = [i.strip() for i in re.split(' +', line)[1:] if i.strip()]
+    for field in l:
+        assert l != None
     return l
 
 
@@ -30,10 +33,11 @@ def read_score_file(filepath):
         next(handle)  # skip the first line dont need it
         header = process_score_file_line(next(handle))
         lines = []
-        while True:
-            line = handle.readline()
+        
+        for line in handle.readlines():
             line = process_score_file_line(line)
             lines.append(line)
+            
 
     return header, lines
 
@@ -54,7 +58,9 @@ def add_string_to_score_file_lines(score_file_header, score_file_lines,
         [type]: [description]
     '''
     score_file_header.append(name)
-    score_file_lines = [line.append(string) for line in score_file_lines]
+    for i, line in enumerate(score_file_lines):
+        score_file_lines[i].append(string)
+    # score_file_lines = [print(line) for line in score_file_lines]
     return score_file_header, score_file_lines
 
 
@@ -63,11 +69,11 @@ def add_result_pdb_to_score_file_lines(score_file_header, score_file_lines,
     name = 'result_pdb'
     result_dir = score_file_path.parent
     descrip_column = score_file_header.index('description')
-    for line in score_file_lines:
+    for i, line in enumerate(score_file_lines):
         descrip = line[descrip_column]
         pdb_path = Path(result_dir).joinpath(Path(descrip).with_suffix('.pdb'))
         assert pdb_path.is_file()
-        line.append(pdb_path)
+        score_file_lines[i].append(pdb_path)
     score_file_header.append('pdb_path')
 
     return score_file_header, score_file_lines
@@ -91,20 +97,22 @@ def add_multi_target_protein_identifiers(target_protein_name, score_file_header,
     return score_file_header, score_file_lines
 
 
-def add_observation_identifiers(results_dir, score_file_path, target_protein_dir=None):
+def add_observation_identifiers(results_dir, score_file_path, score_file_header, score_file_lines, target_protein_dir=None):
     distance_to_parent = 0
     results_dir = Path(results_dir)
     score_file_path = Path(score_file_path)
     score_file_path_copy = score_file_path
+    # print(len(score_file_lines), 'score file lines')
     while score_file_path_copy != results_dir:
         score_file_path_copy = score_file_path_copy.parent
         distance_to_parent += 1
 
     if distance_to_parent == 4:  # this should be a job submitted for multiple target proteins
         target_protein = list(score_file_path.parents)[2].name
-        # score_file_header, score_file_lines = add_multi_target_protein_identifiers(
-        #     target_protein, score_file_header, score_file_lines, target_protein_dir
-        # )
+        score_file_header, score_file_lines = add_multi_target_protein_identifiers(
+             target_protein, score_file_header, score_file_lines, target_protein_dir
+         )
+        print(len(score_file_lines), 'score_file lines')
     ligand_name = score_file_path.parents[1].name
 
     score_file_header, score_file_lines = add_string_to_score_file_lines(
@@ -113,12 +121,9 @@ def add_observation_identifiers(results_dir, score_file_path, target_protein_dir
     return score_file_header, score_file_lines
 
 
-def recursive_score_file_collect(parent_dir, score_file_name='score.sc'):
-    for child in Path(parent_dir).iterdir():
-        if child.is_dir():
-            recursive_score_file_collect(child, score_file_name)
-        elif child.name == score_file_name:
-            yield child.name
+def recursive_score_file_collect(parent, score_file_name='score.sc'):
+    # print('Collecting score files from {}'.format(parent))
+    return Path(parent).rglob("*.sc")
 
 
 def write_list_as_delim(filepath, header, nested_list, delimiter='\t'):
@@ -135,10 +140,13 @@ def write_formated_score_files(results_dir, target_protein_dir,
     score_files = recursive_score_file_collect(results_dir)
     formated_score_files = []
     for sf in score_files:
+        # print('Formating {}'.format(sf))
         sf_header, sf_lines = read_score_file(sf)
+        # print('Read score file', 'Number lines {}'.format(len(sf_lines)))
         sf_header, sf_lines = add_observation_identifiers(
-            results_dir, sf, target_protein_dir
+            results_dir, sf, sf_header, sf_lines, target_protein_dir
         )
+        # print('added observation ids')
         formated_sf_path = Path(sf).with_name(formated_filename)
         write_list_as_delim(formated_sf_path, sf_header, sf_lines)
         formated_score_files.append(formated_sf_path)
@@ -151,7 +159,7 @@ def concatenate_score_files(formated_score_files, target_file):
         for i, formated_score_file in enumerate(formated_score_files):
             with open(str(formated_score_file)) as read_handle:
                 header = next(read_handle)
-                if i == 1:
+                if i == 0:
                     target_handle.write(header)
                 else:
                     for line in read_handle:
